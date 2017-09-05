53c17f0b8dd0a91c80b8ba94931b4ccf500639cb
Christopher Scott <cscott@dwavesys.com>
2017-09-05 13:57:35 -0700

    Removing files from git repository

--- test_all.py
+++
@@ -1,8 +0,0 @@
-''' Runs all unit tests for the `Forecaster` package '''
-
-import unittest
-
-if __name__ == '__main__':
-    suite = unittest.TestLoader().discover('.', pattern = "test_*.py")
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    # unittest.main()
