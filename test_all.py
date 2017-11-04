d1edb8247a55df4c451838eec6f4b466cb42ac7d
Christopher Scott <cscott@dwavesys.com>
2017-11-03 23:10:43 -0700

    Removing custom Money class.

--- test_all.py
+++ test_all.py
@@ -3,6 +3,6 @@
 import unittest
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().discover('.', pattern = "test_*.py")
+    suite = unittest.TestLoader().discover('.', pattern='test_*.py')
     unittest.TextTestRunner(verbosity=2).run(suite)
     # unittest.main()
