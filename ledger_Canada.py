729ae334ea9d07d4a612b473129bbed0c32f49e6
Christopher Scott <christopher@christopherscott.ca>
2017-11-20 22:41:23 -0800

    Moved Debt from ledger_Canada to ledger.

--- ledger_Canada.py
+++ ledger_Canada.py
@@ -574,58 +574,6 @@ class TaxableAccount(Account):
     # tax_credit: Canadian dividend credit
 
 
-class Debt(Account):
-    """ A debt with a balance and an interest rate.
-
-    If there is an outstanding balance, the balance value will be a
-    *negative* value, in line with typical accounting principles.
-
-    Args:
-        minimum_payment (Money): The minimum annual payment on the debt.
-            Optional.
-        reduction_rate (Money, Decimal, float, int): If provided, some
-            (or all) of the debt payments are drawn from savings instead
-            of living expenses. If Money, this is the amount drawn from
-            savings. If a number in [0,1], this is the percentage of the
-            payment that's drawn from savings. Optional.
-        accelerate_payment (bool): If True, payments above the minimum
-            may be made to pay off the balance earlier. Optional.
-    """
-
-    def __init__(self, owner, balance=0, rate=0, transactions={},
-                 nper=1, initial_year=None, settings=Settings,
-                 minimum_payment=Money(0), reduction_rate=1,
-                 accelerate_payment=False):
-        """ Constructor for `Debt`. """
-        super().__init__(
-            owner, balance=balance, rate=rate, transactions=transactions,
-            nper=nper, initial_year=initial_year, settings=settings)
-        self.minimum_payment = Money(minimum_payment)
-        self.reduction_rate = Decimal(reduction_rate) \
-            if reduction_rate is not None \
-            else Settings.DebtReductionRate
-        self.accelerate_payment = bool(accelerate_payment) \
-            if accelerate_payment is not None \
-            else Settings.DebtAcceleratePayment
-
-    def min_inflow(self, when='end', year=None):
-        """ The minimum payment on the debt. """
-        return min(-self.balance_at_time(when, year), self.minimum_payment)
-
-    def max_inflow(self, when='end', year=None):
-        """ The payment at time `when` that would reduce balance to 0.
-
-        This is in addition to any existing payments in the account.
-
-        Example:
-            debt = Debt(-100)
-            debt.maximum_payment('start') == Money(100)  # True
-            debt.add_transaction(100, 'start')
-            debt.maximum_payment('start') == 0  # True
-        """
-        return -self.balance_at_time(when, year)
-
-
 class PrincipleResidence(Account):
     """ A Canadian principle residence. Gains in value are not taxable. """
 
