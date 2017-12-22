b5ab88a8f363189a488245dfe037f7d7f5a8d23a
Christopher Scott <christopher@christopherscott.ca>
2017-12-22 14:32:18 -0800

    Splitting transaction_schedule into two separate modules (transaction and debt_payment) and updating __init__ accordingly.

--- forecaster/strategy/__init__.py
+++ forecaster/strategy/__init__.py
@@ -1,11 +1,11 @@
 # See forecaster.__init__.py for version, author, and licensing info.
 
 __all__ = [
-    'allocation', 'gross_transaction', 'transaction_schedule'
+    'allocation', 'gross_transaction', 'transaction', 'debt_payment'
 ]
 
 from forecaster.strategy.allocation import AllocationStrategy
 from forecaster.strategy.gross_transaction import (
     ContributionStrategy, WithdrawalStrategy)
-from forecaster.strategy.transaction_schedule import (
-    TransactionStrategy, DebtPaymentStrategy)
+from forecaster.strategy.transaction import TransactionStrategy
+from forecaster.strategy.debt_payment import DebtPaymentStrategy
