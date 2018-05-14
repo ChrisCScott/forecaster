d21a2c7799343a0e2f3e2b7b066f1ef341e6d6f4
Christopher Scott <christopher@christopherscott.ca>
2018-05-14 16:54:07 -0700

    Separating object-creation statements from testing statements for easier debugging.

--- tests/canada/test_forecaster.py
+++ tests/canada/test_forecaster.py
@@ -28,7 +28,7 @@ class TestForecasterCanada(TestForecaster):
         forecaster = ForecasterCanada(settings=self.settings)
         assets = copy(forecaster.assets)
         asset = forecaster.add_rrsp()
-        self.assertEqual(asset, RRSP(
+        rrsp = RRSP(
             owner=forecaster.person1,
             balance=Money(0),
             rate=forecaster.allocation_strategy.rate_function(
@@ -40,7 +40,8 @@ class TestForecasterCanada(TestForecaster):
             contribution_room=Money(0),
             contributor=forecaster.person1,
             inflation_adjust=forecaster.scenario.inflation_adjust
-        ))
+        )
+        self.assertEqual(asset, rrsp)
         self.assertEqual(forecaster.assets - assets, {asset})
 
     def test_add_tfsa(self):
@@ -52,7 +53,7 @@ class TestForecasterCanada(TestForecaster):
         forecaster = ForecasterCanada(settings=self.settings)
         assets = copy(forecaster.assets)
         asset = forecaster.add_tfsa()
-        self.assertEqual(asset, TFSA(
+        tfsa = TFSA(
             owner=forecaster.person1,
             balance=Money(0),
             rate=forecaster.allocation_strategy.rate_function(
@@ -64,7 +65,8 @@ class TestForecasterCanada(TestForecaster):
             contribution_room=contribution_accrued,
             contributor=forecaster.person1,
             inflation_adjust=forecaster.scenario.inflation_adjust
-        ))
+        )
+        self.assertEqual(asset, tfsa)
         self.assertEqual(forecaster.assets - assets, {asset})
 
     def test_add_taxable_account(self):
@@ -72,7 +74,7 @@ class TestForecasterCanada(TestForecaster):
         forecaster = ForecasterCanada(settings=self.settings)
         assets = copy(forecaster.assets)
         asset = forecaster.add_taxable_account()
-        self.assertEqual(asset, TaxableAccount(
+        taxable_account = TaxableAccount(
             owner=forecaster.person1,
             balance=Money(0),
             rate=forecaster.allocation_strategy.rate_function(
@@ -82,16 +84,23 @@ class TestForecasterCanada(TestForecaster):
             inputs={},
             initial_year=forecaster.person1.initial_year,
             acb=Money(0)
-        ))
+        )
+        self.assertEqual(asset, taxable_account)
         self.assertEqual(forecaster.assets - assets, {asset})
 
     def test_set_tax_treatment(self):
         """ Test setting tax treatment with Forecaster (Canada). """
-        forecaster = ForecasterCanada(settings=self.settings)
-        self.assertEqual(forecaster.tax_treatment, TaxCanada(
+        forecaster1 = ForecasterCanada(settings=self.settings)
+        tax = TaxCanada(
             inflation_adjust=self.scenario.inflation_adjust,
             province='BC'
-        ))
+        )
+        # Forecaster sometimes prompts tax objects to fill in their
+        # annual data (e.g. tax brackets), so wrap our reference object
+        # in a Forecaster object as well.
+        _ = ForecasterCanada(
+            settings=self.settings, tax_treatment=tax)
+        self.assertEqual(forecaster1.tax_treatment, tax)
 
 if __name__ == '__main__':
     unittest.main()
