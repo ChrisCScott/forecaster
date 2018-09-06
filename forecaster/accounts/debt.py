7bb7c01b3c376a96f1330b102b41389c444d9294
Christopher Scott <christopher@christopherscott.ca>
2018-09-05 23:43:55 -0700

    New `living_expense` property and payment-calulating methods in `Debt`. Updating DebtPaymentStrategy to use new `Debt` methods and new property. Closes #36.

--- forecaster/accounts/debt.py
+++ forecaster/accounts/debt.py
@@ -2,7 +2,6 @@
 
 from decimal import Decimal
 from forecaster.accounts.base import Account
-from forecaster.person import Person
 from forecaster.ledger import Money
 
 class Debt(Account):
@@ -14,10 +13,17 @@ class Debt(Account):
     Args:
         minimum_payment (Money): The minimum annual payment on the debt.
             Optional.
-        reduction_rate (Decimal): The amount of any payment to be drawn
-            from savings instead of living expenses. Expressed as the
-            percentage that's drawn from savings (e.g. 75% drawn from
-            savings would be `Decimal('0.75')`). Optional.
+        living_expense (Money): The amount paid annually out of living
+            expenses. This portion of payments is excluded from
+            the amount determined by `reduction_rate`. If payments for
+            a given year are less than this value, they are 100%
+            excluded from `reduction_rate`. (The total payment amount
+            is not increased to match this value.) Optional.
+        reduction_rate (Decimal): The amount of any payment (in excess
+            of `living_expense`) to be drawn from savings instead of
+            living expenses. Expressed as the percentage that's drawn
+            from savings (e.g. 75% drawn from savings would be
+            `Decimal('0.75')`). Optional.
         accelerated_payment (Money): The maximum value which may be
             paid in a year (above `minimum_payment`) to pay off the
             balance earlier. Optional.
@@ -31,7 +37,9 @@ class Debt(Account):
         self, owner,
         balance=0, rate=0, nper=1,
         inputs=None, initial_year=None, minimum_payment=Money(0),
-        reduction_rate=1, accelerated_payment=Money('Infinity'), **kwargs
+        living_expense=Money(0), reduction_rate=1,
+        accelerated_payment=Money('Infinity'),
+        **kwargs
     ):
         """ Constructor for `Debt`. """
 
@@ -45,6 +53,7 @@ class Debt(Account):
             owner, balance=balance, rate=rate, nper=nper,
             inputs=inputs, initial_year=initial_year, **kwargs)
         self.minimum_payment = Money(minimum_payment)
+        self.living_expense = Money(living_expense)
         self.reduction_rate = Decimal(reduction_rate)
         self.accelerated_payment = Money(accelerated_payment)
 
@@ -83,3 +92,103 @@ class Debt(Account):
                 - self.inflows,
                 Money(0))
         )
+
+    def payment(
+        self, savings_available=Money(0),
+        living_expenses_available=Money('Infinity'),
+        other_payments=Money(0),
+        when='end'
+    ):
+        """ Calculates the maximum payment amount for the year.
+        
+        Args:
+            savings_available (Money): The amount available from
+                savings to repay this debt this year. Optional.
+            living_expenses (Money): The amount available from
+                living expenses to repay this debt this year.
+                Optional.
+            other_payments (Money): An amount of money that is
+                planned for payment to this debt but which has
+                not yet been recorded as an inflow. Optional.
+            when (str, Decimal): The date at which the payment
+                is to occur.
+
+        Returns:
+            Money: The maximum payment amount that can be made to
+                this account this year given the amounts available
+                for repayment.
+        """
+        # Set aside the base amount payable directly from living
+        # expenses (i.e. before drawing down any savings)
+        base_living_expense = max(
+            self.living_expense - (self.inflows + other_payments),
+            Money(0)
+        )
+        # Apply the base living amount right up-front:
+        payment = min(
+            base_living_expense,
+            living_expenses_available
+        )
+        living_expenses_available -= payment
+
+        # Deal with the special cases where we withdraw 0% from
+        # either savings or living expenses (to avoid DIV0 error).
+        # We do the same for both savings and living expenses
+        # because these impose separate limits.
+        if self.reduction_rate == 0:
+            max_savings = Money('Infinity')
+        else:
+            max_savings = savings_available / self.reduction_rate
+        # (This could be indented, but it's much more readable
+        # this way. We can incur an extra comparison if it means
+        # we're being more Pythonic.)
+        if self.reduction_rate == 1:
+            max_living = Money('Infinity')
+        else:
+            max_living = (
+                living_expenses_available / (1 - self.reduction_rate))
+
+        # Available savings and living expenses will result in two
+        # different max payments; use the lesser of the two.
+        payment += min(max_savings, max_living)
+
+        # The payment shouldn't exceed the maximum inflow:
+        return min(
+            self.max_inflow(when) - other_payments,
+            payment
+        )
+
+    def payment_from_savings(self, amount=None, base=Money(0)):
+        """ The amount of annual payments made from savings.
+
+        NOTE: This method is not useful if called on a single
+        transation (if there are multiple inflows for the year)
+        because the `living_expense` amount applies on an annual
+        basis against all transactions, not each transaction
+        individually. Be careful!
+
+        Args:
+            amount (Money): The amount paid against this debt in the
+                year.
+                Optional. If not provided, uses current inflows for
+                the year.
+            base (Money): Amounts already paid against this debt in
+                the year (and which have thus reduced the living
+                expenses portion available to be claimed.)
+                Optional.
+        
+        Returns:
+            Money: The amount paid from savings for the year.
+        """
+        if amount is None:
+            amount = self.inflows
+        # Determine how much of this payment is eligible to be
+        # wholly included in living expenses (after accounting
+        # for past payments, i.e. `base`)
+        living_expense = max(self.living_expense - base, Money(0))
+        payment_from_savings = (
+            (amount - living_expense) * self.reduction_rate
+        )
+
+        # Payments should always be non-negative:
+        return max(Money(0), payment_from_savings)
--- forecaster/strategy/debt_payment.py
+++ forecaster/strategy/debt_payment.py
@@ -49,49 +49,109 @@ class DebtPaymentStrategy(Strategy):
         # timing. (We could convert to `When` here - consider it.)
         self._param_check(self.timing, 'timing', (Decimal, str))
 
+    def _assign_available(self, debt, transactions, available):
+        """ Proposes a transaction based on savings available.
+        
+        Args:
+            debt (Debt): A debt account.
+            transactions (dict[Debt, Money]): Maps debts to
+                payments.
+                `debt` must be a valid key. Its corresponding
+                value may be mutated by this method to reflect
+                an increased payment.
+            available (Money): The amount available to repay
+                debt from savings.
+
+        Returns:
+            Money: The amount of savings (`available`) consumed
+                by the increased payment proposed by this
+                method (via `transactions`).
+        """
+        # Keep track of how much we were paying before adding
+        # a further payment amount:
+        initial_payment = transactions[debt] + debt.inflows
+
+        # Determine how much debt we can repay given how much money
+        # is available:
+        transactions[debt] += debt.payment(
+            savings_available=available,
+            other_payments=transactions[debt],
+            when=self.timing
+        )
+
+        # Return the portion of `available` (i.e. savings)
+        # consumed by this transaction.
+        return debt.payment_from_savings(
+            amount=transactions[debt] - initial_payment,
+            base=initial_payment
+        )
+
+    def _assign_minimums(self, debts, transactions):
+        """ Proposes transactions based on minimum payments.
+        
+        Args:
+            debts (set[Debt]): A set of debt accounts.
+            transactions (dict[Debt, Money]): Maps debts to
+                payments. May be empty. Will be mutated.
+            available (Money): The amount available to repay
+                debt from savings.
+
+        Returns:
+            Money: The amount of savings consumed by the
+                payments proposed by this method.
+                (Proposed payments are returned via
+                `transactions`).
+        """
+        # Figure out the additional inflows necessary to meet minimum
+        # payment obligations:
+        payments = {
+            debt: debt.min_inflow(when=self.timing)
+            for debt in debts
+        }
+
+        # Reduce payments by any already-pending payments (if they exist).
+        # If they don't exist, add an entry so that the following calls
+        # to transaction[debt] don't fail:
+        for debt in debts:
+            if debt not in transactions:
+                transactions[debt] = 0
+            else:
+                payments[debt] -= transactions[debt]
+
+        # Figure out the amount of savings consumed by the proposed
+        # payments:
+        savings_used = sum(
+            debt.payment_from_savings(
+                amount=payments[debt],
+                base=transactions[debt] + debt.inflows)
+            for debt in debts
+        )
+
+        # Mutate transactions to ensure that each debt meets its minimum
+        # payment obligation.
+        for debt in debts:
+            transactions[debt] += payments[debt]
+
+        return savings_used
+
     # pylint: disable=W0613
     @strategy_method('Snowball')
     def strategy_snowball(self, available, debts, *args, **kwargs):
         """ Pays off the smallest debt first. """
         # First, ensure all minimum payments are made.
-        transactions = {
-            debt: debt.min_inflow(when=self.timing)
-            for debt in debts
-        }
+        transactions = {}
+        # Reduce the available amount accordingly, but don't go negative.
+        available -= min(
+            self._assign_minimums(debts, transactions),
+            available)
 
-        available -= sum(transactions[debt] * debt.reduction_rate
-                         for debt in debts)
-
-        if available <= 0:
-            return transactions
-
-        accelerated_debts = {
-            debt for debt in debts
-            if debt.max_inflow(self.timing) > transactions[debt]}
-        # Now we increase contributions to any accelerated debts
-        # (non-accelerated debts can just have minimum payments made,
-        # handled above). Here, increase contributions of the smallest
-        # debt first, then the next, and so on until there's no money
-        # left to allocate to debt repayment:
+        # Increase contributions to debts in order of their balance
+        # (smallest first):
         for debt in sorted(
-            accelerated_debts, key=lambda x: abs(x.balance), reverse=False
+            debts, key=lambda x: abs(x.balance), reverse=False
         ):
-            # Debts that don't reduce savings can be ignored - assume
-            # they're fully repaid in the first year.
-            if debt.reduction_rate == 0:
-                transactions[debt] = debt.max_inflow(self.timing)
-                continue
-
-            # Payment is either the outstanding balance or the total of
-            # the available money remaining for payments
-            payment = min(
-                debt.max_inflow(self.timing) - transactions[debt],  # balance
-                available / debt.reduction_rate  # money available
-            )
-            transactions[debt] += payment
-            # `available` is at least partially taken from savings;
-            # only deduct that portion of the payment from `available`
-            available -= payment * debt.reduction_rate
+            available -= self._assign_available(
+                debt, transactions, available)
 
         return transactions
 
@@ -99,44 +159,34 @@ class DebtPaymentStrategy(Strategy):
     def strategy_avalanche(self, available, debts, *args, **kwargs):
         """ Pays off the highest-interest debt first. """
         # First, ensure all minimum payments are made.
-        transactions = {
-            debt: debt.min_inflow(when=self.timing)
-            for debt in debts
-        }
+        transactions = {}
+        # Reduce the available amount accordingly, but don't go negative.
+        available -= min(
+            self._assign_minimums(debts, transactions),
+            available)
 
-        available -= sum(transactions[debt] * debt.reduction_rate
-                         for debt in debts)
-
-        if available <= 0:
-            return transactions
-
-        accelerated_debts = {
-            debt for debt in debts
-            if debt.max_inflow(self.timing) > transactions[debt]}
-        # Now we increase contributions to any accelerated debts
-        # (non-accelerated debts can just have minimum payments made,
-        # handled above). Here, increase contributions of the largest
-        # rate first, then the next, and so on until there's no money
-        # left to allocate to debt repayment:
+        # Now we increase contributions to debts in order of interest
+        # rate (largest rate first):
         for debt in sorted(
-            accelerated_debts, key=lambda x: x.rate, reverse=True
+            debts, key=lambda x: x.rate, reverse=True
         ):
-            # Debts that don't reduce savings can be ignored - assume
-            # they're fully repaid in the first year.
-            if debt.reduction_rate == 0:
-                transactions[debt] = debt.max_inflow(self.timing)
-                continue
-
-            # Payment is either the outstanding balance or the total of
-            # the available money remaining for payments
-            payment = min(
-                debt.max_inflow(self.timing) - transactions[debt],  # balance
-                available / debt.reduction_rate  # money available
+            # Keep track of how much we were paying before adding
+            # a further payment amount:
+            initial_payment = transactions[debt] + debt.inflows
+            # Determine how much debt we can repay given how much money
+            # is available:
+            transactions[debt] += debt.payment(
+                savings_available=available,
+                other_payments=transactions[debt],
+                when=self.timing
             )
-            transactions[debt] += payment
-            # `available` is at least partially taken from savings;
-            # only deduct that portion of the payment from `available`
-            available -= payment * debt.reduction_rate
+
+            # Reduce the available amount by the portion of the further
+            # payment amount which was drawn from savngs:
+            available -= debt.payment_from_savings(
+                amount=transactions[debt] - initial_payment,
+                base=initial_payment
+            )
 
         return transactions
 
