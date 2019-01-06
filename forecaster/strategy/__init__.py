c1ef633275ea8b0d056b4a9085d46e56e8eedf1f
Christopher Scott <christopher@christopherscott.ca>
2019-01-05 23:58:57 -0800

    Updating imports to reflect changed module name.

--- forecaster/strategy/__init__.py
+++ forecaster/strategy/__init__.py
@@ -3,11 +3,11 @@
 # See forecaster.__init__.py for version, author, and licensing info.
 
 __all__ = [
-    'allocation', 'gross_transaction', 'transaction', 'debt_payment'
+    'allocation', 'gross_transaction', 'account_transaction', 'debt_payment'
 ]
 
 from forecaster.strategy.allocation import AllocationStrategy
 from forecaster.strategy.gross_transaction import (
     LivingExpensesStrategy, WithdrawalStrategy)
-from forecaster.strategy.transaction import AccountTransactionStrategy
+from forecaster.strategy.account_transaction import AccountTransactionStrategy
 from forecaster.strategy.debt_payment import DebtPaymentStrategy
