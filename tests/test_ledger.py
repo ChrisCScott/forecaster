6362404738d1bc95e9cc6033145a84b92121ea5f
Christopher Scott <christopher@christopherscott.ca>
2019-04-15 22:40:39 -0700

    Enabling custom setters and deleters.

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -12,6 +12,7 @@ class TestLedger(Ledger):
         """ Init method that adds a `counter` attribute """
         super().__init__(0)
         self.counter = 0
+        self.side_effect = False
 
     @recorded_property
     def uncached(self):
@@ -19,11 +20,36 @@ class TestLedger(Ledger):
         self.counter += 1
         return self.counter
 
+    @uncached.setter
+    def uncached(self, val):
+        """ Sets `uncached` and toggles `flag` as a side-effect. """
+        self.counter = val
+        self.side_effect = True
+
+    @uncached.deleter
+    def uncached(self):
+        """ Sets counter to 0 and toggles `flag` as a side-effect. """
+        self.counter = 0
+        self.side_effect = True
+
     @recorded_property_cached
     def cached(self):
         """ A recorded property that is cached. """
         self.counter += 1
         return self.counter
+
+    @cached.setter
+    def cached(self, val):
+        """ Sets `cached` and toggles `flag` as a side-effect. """
+        self.counter = val
+        self.side_effect = True
+
+    @cached.deleter
+    def cached(self):
+        """ Toggles `flag` as a side-effect. """
+        # NOTE: We do *not* modify counter here so that we can
+        # more easily confirm that the cache is cleared.
+        self.side_effect = True
 
 
 class TestLedgerMethods(unittest.TestCase):
@@ -33,12 +59,30 @@ class TestLedgerMethods(unittest.TestCase):
         """ Sets up stock attributes for testing. """
         self.ledger = TestLedger()
 
-    def test_uncached(self):
-        """ Tests uncached properties. """
+    def test_uncached_get(self):
+        """ Tests getting uncached properties. """
         self.assertEqual(self.ledger.uncached, 1)
         self.assertEqual(self.ledger.uncached, 2)
 
-    def test_cached(self):
+    def test_uncached_set(self):
+        """ Tests setting uncached properties. """
+        self.ledger.uncached = 10
+        # Should return 1 more because `fget` increments `counter`:
+        self.assertEqual(self.ledger.uncached, 11)
+        # Side effect should be observed:
+        self.assertTrue(self.ledger.side_effect)
+
+    def test_uncached_del(self):
+        """ Tests deleting uncached properties. """
+        self.ledger.counter = 10
+        # Deleting `uncached` should reset `counter` to 0:
+        del self.ledger.uncached
+        # Should return 1 because `fget` increments `counter`:
+        self.assertEqual(self.ledger.uncached, 1)
+        # Side effect should be observed:
+        self.assertTrue(self.ledger.side_effect)
+
+    def test_cached_get(self):
         """ Tests cached properties. """
         # `cached` will be cached when it is first called:
         self.ledger.counter = 5
@@ -47,6 +91,34 @@ class TestLedgerMethods(unittest.TestCase):
         # subsequent call no matter what `counter` is:
         self.ledger.counter = 10
         self.assertEqual(self.ledger.cached, 6)
+
+    def test_cached_set(self):
+        """ Tests setting cached properties. """
+        self.ledger.cached = 10
+        # Should return 1 more because `fget` increments `counter`:
+        self.assertEqual(self.ledger.cached, 11)
+        # (Try again to ensure setting doesn't interfere with cached
+        # behaviour)
+        self.assertEqual(self.ledger.cached, 11)
+        # Side effect should be observed:
+        self.assertTrue(self.ledger.side_effect)
+
+    def test_cached_del(self):
+        """ Tests deleting cached properties. """
+        self.ledger.counter = 10
+        # Call `cached` to ensure it's cached (should be 11):
+        _ = self.ledger.cached
+        # Call `cached` again (should be unchanged, since it's cached):
+        _ = self.ledger.cached
+        # Deleting `cached` should uncache it:
+        del self.ledger.cached
+        # Should now return 12 because the cache is cleared:
+        self.assertEqual(self.ledger.cached, 12)
+        # (Try again to ensure setting doesn't interfere with cached
+        # behaviour)
+        self.assertEqual(self.ledger.cached, 12)
+        # Side effect should be observed:
+        self.assertTrue(self.ledger.side_effect)
 
     def test_clear_cache(self):
         """ Clears cache """
