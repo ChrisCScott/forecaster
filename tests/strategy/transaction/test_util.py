64709c365d1f8b00720803a8b13e8bd5027149c5
Christopher Scott <christopher@christopherscott.ca>
2019-07-09 21:57:36 -0700

    Removing TODO comments from empty test files.

--- tests/strategy/transaction/test_util.py
+++ tests/strategy/transaction/test_util.py
@@ -1,3 +1,6 @@
-""" TODO """
+""" Tests for `util.py` """
 
-# TODO: Add tests for helper methods.
+# NOTE: All functionality of this module is tested indirectly by
+# `test_base.py` (via tests for TransactionTraversal).
+# For now, we aren't adding separate tests for `util.py` methods,
+# since it doesn't improve code coverage.
