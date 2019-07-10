c482641688520cc7fa6c3746228c18306c8850a7
Christopher Scott <christopher@christopherscott.ca>
2019-07-09 21:57:36 -0700

    Removing TODO comments from empty test files.

--- tests/strategy/transaction/test_node.py
+++ tests/strategy/transaction/test_node.py
@@ -1,3 +1,6 @@
-""" TODO """
+""" Tests for node.py """
 
-# TODO: Add tests for TransactionNode.
+# NOTE: All functionality of this module is tested indirectly by
+# `test_base.py` (via tests for TransactionTraversal).
+# For now, we aren't adding separate tests for `node.py` methods,
+# since it doesn't improve code coverage.
