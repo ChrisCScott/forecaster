663ecc5d1d7793b16611a0bd062c5dea2fdf8af8
Christopher Scott <christopher@christopherscott.ca>
2021-11-09 16:51:47 -0800

    Adding tests for utility.precision.
    Also splitting up tests for other utility classes/methods.
    
    Some monolithic tests have been split up into several smaller tests.
    Some references to Decimal are removed from tests and
    moved to dedicated high-precision testing methods.

--- tests/utility/test_timing.py
+++ tests/utility/test_timing.py
@@ -1,10 +1,8 @@
-""" Tests free methods and classes in the utility module. """
+""" Tests free methods and classes in the utility.timing module. """
 
 import unittest
-from forecaster.utility import (
-    Timing, transactions_from_timing,
-    nearest_year, extend_inflation_adjusted,
-    when_conv, frequency_conv)
+from forecaster.utility.timing import (
+    Timing, transactions_from_timing, when_conv, frequency_conv)
 
 class TestTiming(unittest.TestCase):
     """ A test case for Timing. """
@@ -294,55 +292,6 @@ class TestFreeMethods(unittest.TestCase):
         """ Test setting frequency to 'invalid', an invalid value. """
         with self.assertRaises(ValueError):
             _ = frequency_conv('invalid')
-
-    def test_nearest_year(self):
-        """ Tests nearest_year(). """
-        vals = {1999: 2, 2001: 4, 2003: 8, 2006: 16}
-
-        self.assertEqual(nearest_year(vals, 1998), 1999)
-        self.assertEqual(nearest_year(vals, 1999), 1999)
-        self.assertEqual(nearest_year(vals, 2000), 1999)
-        self.assertEqual(nearest_year(vals, 2001), 2001)
-        self.assertEqual(nearest_year(vals, 2002), 2001)
-        self.assertEqual(nearest_year(vals, 2003), 2003)
-        self.assertEqual(nearest_year(vals, 2004), 2003)
-        self.assertEqual(nearest_year(vals, 2005), 2003)
-        self.assertEqual(nearest_year(vals, 2006), 2006)
-        self.assertEqual(nearest_year(vals, 2007), 2006)
-
-        self.assertEqual(nearest_year({}, 2000), None)
-
-    def test_extend_inflation_adjusted(self):
-        """ Tests extend_inflation_adjusted(). """
-        inf = {
-            1998: 0.25,
-            1999: 0.5,
-            2000: 0.75,
-            2001: 1,
-            2002: 2}
-        vals = {1999: 2, 2001: 4, 2003: 8}
-
-        def inflation_adjust(target_year, base_year):
-            """ Inflation from base_year to target_year. """
-            return inf[target_year] / inf[base_year]
-
-        # Test each year from 1997 to 2004:
-        with self.assertRaises(KeyError):
-            extend_inflation_adjusted(vals, inflation_adjust, 1997)
-        self.assertEqual(extend_inflation_adjusted(
-            vals, inflation_adjust, 1998), 1)
-        self.assertEqual(extend_inflation_adjusted(
-            vals, inflation_adjust, 1999), 2)
-        self.assertEqual(extend_inflation_adjusted(
-            vals, inflation_adjust, 2000), 3)
-        self.assertEqual(extend_inflation_adjusted(
-            vals, inflation_adjust, 2001), 4)
-        self.assertEqual(extend_inflation_adjusted(
-            vals, inflation_adjust, 2002), 8)
-        self.assertEqual(extend_inflation_adjusted(
-            vals, inflation_adjust, 2003), 8)
-        with self.assertRaises(KeyError):
-            extend_inflation_adjusted(vals, inflation_adjust, 2004)
 
     # TODO: Test build_inflation_adjust and transactions_from_timing
 
