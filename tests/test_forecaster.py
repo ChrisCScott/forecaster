69c9299c6f0ee264798cba1ebb7c6804dce2722c
Christopher Scott <christopher@christopherscott.ca>
2021-11-12 21:11:51 -0800

    Removing use of Decimal for generic test methods.
    Adds one new test specifically for testing Decimal inputs.
    That test currently fails - no surprise, as Forecaster is not yet
    updated for high-precision support.
    All other tests pass.
    
    Relates to #77.

--- tests/test_forecaster.py
+++ tests/test_forecaster.py
@@ -26,7 +26,7 @@ class TestForecaster(unittest.TestCase):
         self.settings.num_years = 2
         self.settings.living_expenses_strategy = (
             LivingExpensesStrategy.strategy_const_contribution)
-        self.settings.living_expenses_base_amount = Decimal(1000)
+        self.settings.living_expenses_base_amount = 1000
 
         # Allow subclasses to use subclasses of Forecaster by assigning
         # to forecaster_type
@@ -79,18 +79,116 @@ class TestForecaster(unittest.TestCase):
             name="Test 1",
             birth_date="1 January 1980",
             retirement_date="31 December 2040",
-            gross_income=Decimal(10000),
-            raise_rate=Decimal(0),
+            gross_income=10000,
+            raise_rate=0,
             spouse=None,
             tax_treatment=self.tax_treatment)
         # An account with $1000 in it (and no interest)
         self.account = Account(
             owner=self.person,
-            balance=Decimal(1000))
+            balance=1000)
         # A debt with a $100 balance (and no interest)
         self.debt = Debt(
             owner=self.person,
-            balance=Decimal(100))
+            balance=100)
+
+        # Init a Forecaster object here for convenience:
+        self.forecaster = self.forecaster_type(settings=self.settings)
+
+    def setUp_decimal(self):
+        """ Builds default strategies/persons/etc. with Decimal inputs. """
+        # Use a default settings object:
+        # (This is conditional so that subclasses can assign their own
+        # settings object before calling super().setUp())
+        if not hasattr(self, 'settings'):
+            self.settings = Settings()
+
+        # To simplify tests, modify Settings so that forecasts are
+        # just 2 years with easy-to-predict contributions ($1000/yr)
+        self.settings.num_years = 2
+        self.settings.living_expenses_strategy = (
+            LivingExpensesStrategy.strategy_const_contribution)
+        self.settings.living_expenses_base_amount = Decimal(1000)
+
+        # Allow subclasses to use subclasses of Forecaster by assigning
+        # to forecaster_type
+        if not hasattr(self, 'forecaster_type'):
+            self.forecaster_type = Forecaster
+
+        # Build default `SubForecast` inputs based on `settings`:
+        self.initial_year = self.settings.initial_year
+        self.scenario = Scenario(
+            inflation=Decimal(self.settings.inflation),
+            stock_return=Decimal(self.settings.stock_return),
+            bond_return=Decimal(self.settings.bond_return),
+            other_return=Decimal(self.settings.other_return),
+            management_fees=Decimal(self.settings.management_fees),
+            initial_year=self.settings.initial_year,
+            num_years=self.settings.num_years)
+        self.living_expenses_strategy = LivingExpensesStrategy(
+            strategy=self.settings.living_expenses_strategy,
+            base_amount=Decimal(self.settings.living_expenses_base_amount),
+            rate=Decimal(self.settings.living_expenses_rate),
+            inflation_adjust=self.scenario.inflation_adjust)
+        self.saving_strategy = TransactionStrategy(
+            strategy=self.settings.saving_strategy,
+            weights={
+                year: Decimal(val) for (year, val) in
+                self.settings.saving_weights.items()})
+        self.withdrawal_strategy = TransactionStrategy(
+            strategy=self.settings.withdrawal_strategy,
+            weights={
+                year: Decimal(val) for (year, val) in
+                self.settings.withdrawal_weights.items()})
+        self.allocation_strategy = AllocationStrategy(
+            strategy=self.settings.allocation_strategy,
+            min_equity=Decimal(self.settings.allocation_min_equity),
+            max_equity=Decimal(self.settings.allocation_max_equity),
+            target=Decimal(self.settings.allocation_target),
+            standard_retirement_age=(
+                self.settings.allocation_std_retirement_age),
+            risk_transition_period=self.settings.allocation_risk_trans_period,
+            adjust_for_retirement_plan=(
+                self.settings.allocation_adjust_retirement))
+        self.debt_payment_strategy = DebtPaymentStrategy(
+            strategy=self.settings.debt_payment_strategy,
+            high_precision=Decimal)
+        self.tax_treatment = Tax(
+            tax_brackets={
+                year: {Decimal(lower): Decimal(upper)}
+                for (year, vals) in self.settings.tax_brackets.items()
+                for (lower, upper) in vals.items()},
+            personal_deduction={
+                year: Decimal(val) for (year, val) in
+                self.settings.tax_personal_deduction.items()},
+            credit_rate={
+                year: Decimal(val) for (year, val) in
+                self.settings.tax_credit_rate.items()},
+            inflation_adjust=self.scenario.inflation_adjust,
+            high_precision=Decimal)
+
+        # Now build some Ledger objects to test against:
+        # A person making $10,000/yr
+        self.person = Person(
+            initial_year=self.initial_year,
+            name="Test 1",
+            birth_date="1 January 1980",
+            retirement_date="31 December 2040",
+            gross_income=Decimal(10000),
+            raise_rate=Decimal(0),
+            spouse=None,
+            tax_treatment=self.tax_treatment,
+            high_precision=Decimal)
+        # An account with $1000 in it (and no interest)
+        self.account = Account(
+            owner=self.person,
+            balance=Decimal(1000),
+            high_precision=Decimal)
+        # A debt with a $100 balance (and no interest)
+        self.debt = Debt(
+            owner=self.person,
+            balance=Decimal(100),
+            high_precision=Decimal)
 
         # Init a Forecaster object here for convenience:
         self.forecaster = self.forecaster_type(settings=self.settings)
@@ -107,7 +205,8 @@ class TestForecaster(unittest.TestCase):
             super().assertEqual(first, second)
         # Then recursively check each pair of values:
         for key in first:
-            self.assertEqual(first[key], second[key], msg=msg, memo=memo)
+            self.assertEqual( #@IgnoreException
+                first[key], second[key], msg=msg, memo=memo) #@IgnoreException
 
     def assertEqual_list(self, first, second, msg=None, memo=None):
         """ Extends equality testing for lists with complex members. """
@@ -144,7 +243,8 @@ class TestForecaster(unittest.TestCase):
                     # added to the memo, so make a copy (which will
                     # be discarded if the objects are not equal).
                     memo_copy = copy(memo)
-                    self.assertEqual(val1, val2, msg=msg, memo=memo_copy)
+                    self.assertEqual( #@IgnoreException
+                        val1, val2, msg=msg, memo=memo_copy)
                 except AssertionError:
                     # If we didn't find a match, advance to the next
                     # value in second and try that.
@@ -167,7 +267,7 @@ class TestForecaster(unittest.TestCase):
         # pylint: disable=invalid-name
 
         # For complicated objects, recurse onto the attributes dict:
-        self.assertEqual(
+        self.assertEqual( #@IgnoreException
             first.__dict__, second.__dict__, msg=msg, memo=memo)
 
     def assertEqual(self, first, second, msg=None, memo=None):
@@ -199,7 +299,7 @@ class TestForecaster(unittest.TestCase):
         try:
             # If these are equal under ordinary comparison, accept that
             # and don't so any further special testing.
-            super().assertEqual(first, second, msg=msg)
+            super().assertEqual(first, second, msg=msg) #@IgnoreException
             return
         except AssertionError as error:
             # If the superclass assertEqual doesn't find equality, run
@@ -216,9 +316,10 @@ class TestForecaster(unittest.TestCase):
                     not isinstance(first, type(second)) and
                     not isinstance(second, type(first))
             ):
-                raise error
+                raise error #@IgnoreException
             elif isinstance(first, dict):
-                self.assertEqual_dict(first, second, msg=msg, memo=memo)
+                self.assertEqual_dict( #@IgnoreException
+                    first, second, msg=msg, memo=memo)
             elif isinstance(first, collections.abc.Sequence):
                 self.assertEqual_list(first, second, msg=msg, memo=memo)
             elif isinstance(first, collections.abc.Iterable):
@@ -227,7 +328,7 @@ class TestForecaster(unittest.TestCase):
                 self.assertEqual_complex(first, second, msg=msg, memo=memo)
             else:
                 # If none of our special tests apply, accept the error.
-                raise error
+                raise error #@IgnoreException
 
     def assertNotEqual(self, first, second, msg=None):
         """ Overloaded to test non-equality of complex objects. """
@@ -301,6 +402,63 @@ class TestForecaster(unittest.TestCase):
         self.forecaster = Forecaster(
             living_expenses_strategy=LivingExpensesStrategy(
                 strategy=LivingExpensesStrategy.strategy_const_contribution,
+                base_amount=500, inflation_adjust=None),
+            settings=self.settings)
+        forecast = self.forecaster.run_forecast(
+            people={self.person},
+            accounts={self.account},
+            debts={})
+
+        # Test that it starts and ends in the right place and that
+        # income and total balance (principal) are correct
+        self.assertEqual(
+            forecast.scenario, self.scenario)
+        # Pylint has trouble with attributes added by metaclass
+        # pylint: disable=no-member
+        self.assertEqual(
+            len(forecast.principal_history), self.scenario.num_years)
+        # pylint: enable=no-member
+
+        # Test that the $500 in contributions have been added to the
+        # initial $1000 principal by the start of year 2:
+        self.assertAlmostEqual(
+            forecast.principal,
+            1500,
+            places=2)
+        # Gross income should be unchanged at $10,000:
+        self.assertAlmostEqual(
+            forecast.income_forecast.gross_income,
+            10000,
+            places=2)
+
+    def test_run_forecast_mutation(self):
+        """ Test that Forecaster.run_forecast doesn't mutate arguments. """
+        # Run a forecast and check whether the inputs were mutated:
+        forecast = self.forecaster.run_forecast(
+            people={self.person},
+            accounts={self.account},
+            debts={self.debt})
+        # The originally-provided Person's history dicts should have
+        # length 1 (since they haven't been mutated). They should be
+        # length 2 for the Person held by the Forecast.
+        # pylint: disable=no-member
+        self.assertEqual(len(self.person.gross_income_history), 1)
+        # pylint: enable=no-member
+        self.assertEqual(
+            len(next(iter(forecast.people)).gross_income_history), 2)
+
+    def test_decimal(self):
+        """ Test Forecaster.run_forecast with Decimal arguments. """
+        # Convert values to Decimal:
+        self.setUp_decimal()
+
+        # This test is based on test_run_forecast_basic
+
+        # Run a simple forecast with $10,000 income, $500 in annual
+        # contributions, and $1000 in starting balances with no growth:
+        self.forecaster = Forecaster(
+            living_expenses_strategy=LivingExpensesStrategy(
+                strategy=LivingExpensesStrategy.strategy_const_contribution,
                 base_amount=Decimal(500), inflation_adjust=None),
             settings=self.settings)
         forecast = self.forecaster.run_forecast(
@@ -329,22 +487,6 @@ class TestForecaster(unittest.TestCase):
             forecast.income_forecast.gross_income,
             Decimal(10000),
             places=2)
-
-    def test_run_forecast_mutation(self):
-        """ Test that Forecaster.run_forecast doesn't mutate arguments. """
-        # Run a forecast and check whether the inputs were mutated:
-        forecast = self.forecaster.run_forecast(
-            people={self.person},
-            accounts={self.account},
-            debts={self.debt})
-        # The originally-provided Person's history dicts should have
-        # length 1 (since they haven't been mutated). They should be
-        # length 2 for the Person held by the Forecast.
-        # pylint: disable=no-member
-        self.assertEqual(len(self.person.gross_income_history), 1)
-        # pylint: enable=no-member
-        self.assertEqual(
-            len(next(iter(forecast.people)).gross_income_history), 2)
 
 
 if __name__ == '__main__':
