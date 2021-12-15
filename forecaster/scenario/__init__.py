3c26af4e549390649a630cbdd03a824e052be66a
Christopher Scott <christopher@christopherscott.ca>
2021-12-14 23:21:15 -0800

    Updating `__init__.py` with new submodules.
    This adds `scenario_sampler`, `samplers`, and `util` to the
    import chain for the `scenario` module.

--- forecaster/scenario/__init__.py
+++ forecaster/scenario/__init__.py
@@ -3,8 +3,14 @@
 # See forecaster.__init__.py for version, author, and licensing info.
 
 __all__ = [
-    'scenario', 'historical_value_reader'
+    'scenario', 'scenario_sampler', 'samplers',
+    'historical_value_reader', 'util'
 ]
 
 from forecaster.scenario.scenario import Scenario, InflationAdjust
+from forecaster.scenario.scenario_sampler import ScenarioSampler
+from forecaster.scenario.samplers import MultivariateSampler, WalkForwardSampler
 from forecaster.scenario.historical_value_reader import HistoricalValueReader
+from forecaster.scenario.util import (
+    regularize_returns, return_for_date_from_values, return_over_period,
+    returns_for_dates_from_values, values_from_returns)
